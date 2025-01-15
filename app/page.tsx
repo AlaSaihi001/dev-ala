d1e80a05ce03785c9f8831d01a56c0207477ae67
ala saihi <alawork001@gmail.com>
2025-01-15 20:36:23 +0100

    ESLint and Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-const Home = () => {
-  return <h1 className="text-3xl text-white font-black">Welecome</h1>;
-};
+const Home = () => (
+  <h1 className="text-3xl text-white font-black">Welecome</h1>
+);
 
 export default Home;
