b1a4b31539cf941d447eecf8843b9bdc921982c2
ala saihi <alawork001@gmail.com>
2025-01-24 16:33:40 +0100

    add fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 const Home = () => (
-  <h1 className="h1-bold">
-    Welecome
-  </h1>
+  <>
+    <h1 className="h1-bold">Welecome</h1>
+    <h2 className="h1-bold font-inter">Welecome, inter</h2>
+    <h2 className="h1-bold font-space-grotesk">Welecome</h2>
+  </>
 );
 
 export default Home;
