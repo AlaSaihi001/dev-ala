bde992db661ec1026c8f3d8045280184b863ea70
ala saihi <alawork001@gmail.com>
2025-01-24 18:27:14 +0100

    Create auth roots

---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Welecome</h1>
+  </>
+);
+
+export default Home;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter, Space_Grotesk as SpaceGrotesk } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import { ThemeProvider } from "next-themes";
-import Navbar from "@/components/navigation/navbar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -42,9 +41,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
           {children}
-
         </ThemeProvider>
       </body>
     </html>
