0d9638ea28317aeac196dc1610db8e30a5dfeca3
ala saihi <alawork001@gmail.com>
2025-01-27 17:04:59 +0100

    Creating Auth Form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
 import React from "react";
 
 const SignUp = () => {
-  return <div>Signup</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", username: "" }}
+      onSubmit={(data) =>
+        Promise.resolve({
+          success: true,
+          data,
+        })
+      }
+    />
+  );
 };
 
 export default SignUp;
